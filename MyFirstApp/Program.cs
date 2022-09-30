a18ecbbd0ad629754af3e72fbd7fd4ccaa6d8710
Kätriin Julle <katriinjulle@gmail.com>
2022-09-30 17:53:46 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// This is a comment
+
+Console.WriteLine("Enter your name: ");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + userName + "!");
+//String interpolation
+Console.WriteLine($"Hello, {userName}!");
